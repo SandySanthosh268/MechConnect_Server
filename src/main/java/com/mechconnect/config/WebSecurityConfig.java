8ca04f42bfebafeb1a7e54c12516efa677667a1b
NARSI NANDHU <santhoshayyanar268@gmail.com>
2026-03-16 19:15:25 +0530

    fix render port configuration-11

--- src/main/java/com/mechconnect/config/WebSecurityConfig.java
+++ src/main/java/com/mechconnect/config/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173","https://mech-connect-nine.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
